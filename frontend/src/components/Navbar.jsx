c2c806db2cd8a2783414dd69190c1bdbffc2d57c
sntripathi180 <139564261+sntripathi180@users.noreply.github.com>
2025-08-12 12:08:15 +0530

    UI : create myProfile,myAppointments

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img onClick={()=>{navigate('/')}} className="w-44 cursor-pointer" src={assets.logo} alt="logo" />
-      <ul className="hid:\2.study\cohort 2\photo-1.jpeg d:\2.study\cohort 2\join-2.webp d:\2.study\cohort 2\join-4.webp d:\2.study\cohort 2\change-1.webp d:\2.study\cohort 2\photo-2.webp d:\2.study\cohort 2\2.webp d:\2.study\cohort 2\join-3.webp d:\2.study\cohort 2\about-2.webp d:\2.study\cohort 2\about-1.jpeg d:\2.study\cohort 2\join-1.webp d:\2.study\cohort 2\photo-5.jpg d:\2.study\cohort 2\photo-4.jpg d:\2.study\cohort 2\photo-3.jpgdden md:flex  items-start gap-5 font-medium">
+      <ul className="hidden md:flex  items-start gap-5 font-medium">
         <NavLink to="/">
           <li className="py-1 ">HOME</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
@@ -71,6 +71,22 @@ const Navbar = () => {
             Create Account
           </button>
         )}
+        <img onClick={()=>{setShowMenu(true)}} className="w-6 md:hidden" src={assets.menu_icon} alt="" />
+        {/* -----Mobile menu */}
+
+        <div className={`${showMenu ? 'fixed w-full ': 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+          <div className="flex items-center justify-between px-5 py-6">
+            <img className="w-36" src={assets.logo} alt="" />
+            <img className="w-7" onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+          </div> 
+          <ul className="flex flex-col items-center gap-2 text-lg font-medium">
+            <NavLink  onClick={()=>setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
+            <NavLink onClick={()=>setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+          </ul>
+
+        </div>
       </div>
     </div>
   );
